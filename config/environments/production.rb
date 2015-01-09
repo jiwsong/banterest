288006737dba6df77e5948e2ba1a48a88536e200
Jiwoo Song <jiwsong@gmail.com>
2015-01-08 16:20:56 -0800

    Fixed AWS bucket name for Heroku storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'http://banterest.herokuapp.com/' }
   
+  # Sets Paperclip to upload images to AWS S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
