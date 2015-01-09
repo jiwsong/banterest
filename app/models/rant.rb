312e7a5ac89266b93fe9e50849fc7418a483f065
Jiwoo Song <jiwsong@gmail.com>
2015-01-08 17:33:53 -0800

    Added jQuery Masonry

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -1,6 +1,6 @@
 class Rant < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "200x200>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
