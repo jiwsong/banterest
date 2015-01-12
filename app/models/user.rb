8df897ce9c6602f417d32a4253ad266eac4414f2
Jiwoo Song <jiwsong@gmail.com>
2015-01-12 14:56:00 -0800

    Fixed bug so that all rants deleted once user cancels account

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable       
 
-  has_many :rants
+  has_many :rants, dependent: :destroy
 
   validates :name, presence: true
 end
