f6baabe9957efd722b2711ca4c2840e381ecfa9e
Jiwoo Song <jiwsong@gmail.com>
2015-01-12 14:45:15 -0800

    Migrated name column to users, updated rants to show user name instead of email

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -3,4 +3,5 @@ class Rant < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "200x200>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates :description, presence: true
 end
